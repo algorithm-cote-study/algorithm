c58b6311ddfa8b72e0aedaa8c2445f7bdaadac30
yeji <choyeji1591@gmail.com>
2022-12-18 21:22:02 +0900

    feat: 인프런 2주차 풀이

--- src/main/java/sgyj/inflearn/yeji/week2/Solution7.java
+++ src/main/java/sgyj/inflearn/yeji/week2/Solution7.java
@@ -5,23 +5,32 @@ import java.util.stream.IntStream;
 
 public class Solution7 {
 
+    /*
+    * @description
+    * 자연수 N이 입력되면 1부터 N까지의 소수의 개수를 출력하는 프로그램을 작성하세요.
+    * 만약 20이 입력되면 1부터 20까지의 소수는 2, 3, 5, 7, 11, 13, 17, 19로 총 8개입니다.
+    *
+    * @input
+    * 첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+    * ex) 20
+    * @output
+    * 첫 줄에 자연수의 개수 N(2<=N<=200,000)이 주어집니다.
+    * ex) 8
+    * */
     public static void main(String[] args){
         Scanner in=new Scanner( System.in);
         int input1 = in.nextInt();
-        in.nextLine();
-        int[] ints = IntStream.rangeClosed( 1, input1 ).toArray();
-        int answer = 0;
-        for(int i=0; i<ints.length; i++){
-            if(ints[i] == 0) continue;
-
-            for(int j=2*i; j<ints.length; j+=i){
-                ints[j] = 0;
+        int answer = -1;
+        int[] checkPrime = IntStream.rangeClosed( 0,input1 ).toArray();
+        for(int i=2; i<=input1; i++){
+            for(int j = i+i; j<=input1; j+=i){
+                checkPrime[j] = 0;
             }
+        }
+        for(int prime : checkPrime){
+            if(prime != 0) answer++;
+        }
 
-        }
-        for ( int anInt : ints ) {
-            if(anInt != 0) answer++;
-        }
-        System.out.println( "solution = " + answer );
+        System.out.println( answer );
     }
 }
