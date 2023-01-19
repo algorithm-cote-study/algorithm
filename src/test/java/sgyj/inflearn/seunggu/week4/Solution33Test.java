0a0cd7bab45d678d85a7f7eec7d08b84a2a3eca9
leeseunggu <leesg107@naver.com>
2023-01-19 12:19:00 +0900

    feat: 4주차 문제 풀이 완료

--- src/test/java/sgyj/inflearn/seunggu/week4/Solution33Test.java
+++ src/test/java/sgyj/inflearn/seunggu/week4/Solution33Test.java
@@ -3,6 +3,7 @@ package sgyj.inflearn.seunggu.week4;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sgyj.inflearn.common.TestFileUtil;
@@ -14,22 +15,24 @@ class Solution33Test {
     @DisplayName( "매출액의 종류 테스트 케이스 1" )
     void test_case_1 () throws Exception {
         // given
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution33/test_case1.txt" );
-        // when
-        String solution = Solution33.solution( reader );
+        String solution = getSolution( "static/solution33/test_case1.txt" );
 
         // then
         assertEquals( "3 3 3 3 3 3 3 3 ", solution );
 
     }
 
+    private String getSolution ( String fileName ) throws IOException {
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), fileName );
+        // when
+        return Solution33.solution( reader );
+    }
+
     @Test
     @DisplayName( "매출액의 종류 테스트 케이스 2" )
     void test_case_2 () throws Exception {
         // given
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution33/test_case2.txt" );
-        // when
-        String solution = Solution33.solution( reader );
+        String solution = getSolution( "static/solution33/test_case2.txt" );
 
         // then
         assertEquals( "7 7 8 8 9 9 10 10 10 10 10 ", solution );
@@ -40,9 +43,8 @@ class Solution33Test {
     @DisplayName( "매출액의 종류 테스트 케이스 3" )
     void test_case_3 () throws Exception {
         // given
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution33/test_case3.txt" );
-        // when
-        String solution = Solution33.solution( reader );
+        String solution = getSolution( "static/solution33/test_case3.txt" );
+        BufferedReader reader;
 
         // then
         reader = TestFileUtil.getReader( this.getClass(), "static/solution33/answer3.txt" );
@@ -61,9 +63,8 @@ class Solution33Test {
     @DisplayName( "매출액의 종류 테스트 케이스 4" )
     void test_case_4 () throws Exception {
         // given
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution33/test_case4.txt" );
-        // when
-        String solution = Solution33.solution( reader );
+        String solution = getSolution( "static/solution33/test_case4.txt" );
+        BufferedReader reader;
 
         reader = TestFileUtil.getReader( this.getClass(), "static/solution33/answer4.txt" );
         StringBuilder stringBuilder = new StringBuilder();
@@ -81,9 +82,8 @@ class Solution33Test {
     @DisplayName( "매출액의 종류 테스트 케이스 5" )
     void test_case_5 () throws Exception {
         // given
-        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution33/test_case5.txt" );
-        // when
-        String solution = Solution33.solution( reader );
+        String solution = getSolution( "static/solution33/test_case5.txt" );
+        BufferedReader reader;
 
         reader = TestFileUtil.getReader( this.getClass(), "static/solution33/answer5.txt" );
         StringBuilder stringBuilder = new StringBuilder();
---
+++ src/test/java/sgyj/inflearn/seunggu/week4/Solution40Test.java
@@ -0,0 +1,78 @@
+package sgyj.inflearn.seunggu.week4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.BufferedReader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import sgyj.inflearn.common.TestFileUtil;
+
+@DisplayName( "후위식 연산(postfix)" )
+class Solution40Test {
+
+    @Test
+    @DisplayName( "후위식 연산(postfix) 테스트 케이스 1" )
+    void test_case_1 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution40/test_case1.txt" );
+        // when
+        int solution = Solution40.solution( reader );
+
+        // then
+        assertEquals( 17, solution );
+
+    }
+
+    @Test
+    @DisplayName( "후위식 연산(postfix) 테스트 케이스 2" )
+    void test_case_2 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution40/test_case2.txt" );
+        // when
+        int solution = Solution40.solution( reader );
+
+        // then
+        assertEquals( 24, solution );
+
+    }
+
+    @Test
+    @DisplayName( "후위식 연산(postfix) 테스트 케이스 3" )
+    void test_case_3 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution40/test_case3.txt" );
+        // when
+        int solution = Solution40.solution( reader );
+
+        // then
+        assertEquals( 48, solution );
+
+    }
+
+    @Test
+    @DisplayName( "후위식 연산(postfix) 테스트 케이스 4" )
+    void test_case_4 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution40/test_case4.txt" );
+        // when
+        int solution = Solution40.solution( reader );
+
+        // then
+        assertEquals( 57, solution );
+
+    }
+
+    @Test
+    @DisplayName( "후위식 연산(postfix) 테스트 케이스 5" )
+    void test_case_5 () throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader( this.getClass(), "static/solution40/test_case5.txt" );
+        // when
+        int solution = Solution40.solution( reader );
+
+        // then
+        assertEquals( 342, solution );
+
+    }
+
+}
