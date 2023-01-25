6847d8f29348132132df672fc19cb4a87bb22ca6
yeji.jo <choyeji1591@gmail.com>
2023-01-25 21:00:42 +0900

    feat : 인프런 6주차 DFS,BFS

---
+++ src/main/java/sgyj/inflearn/yeji/week6/Solution55.java
@@ -0,0 +1,20 @@
+package sgyj.inflearn.yeji.week6;
+
+import java.util.Scanner;
+
+public class Solution55 {
+    // 이진수 출력 (재귀) Integer.toBinaryString();
+    public static void solution( int n){
+        if(n!=0){
+            solution( n/2 );
+            System.out.print(n%2);
+        }
+
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner( System.in);
+        int n = sc.nextInt();
+        solution( n );
+    }
+}
