d68781db53cd2e4207e4cb9be88a3879463478a6
leeseunggu <leesg107@naver.com>
2023-01-29 14:08:33 +0900

    feat: 디렉토리 구조 변경

--- src/main/java/sgyj/inflearn/yeji/section7/Solution9.java
+++ src/main/java/sgyj/inflearn/yeji/section7/Solution9.java
@@ -2,7 +2,7 @@ package sgyj.inflearn.yeji.section7;
 
 import sgyj.inflearn.yeji.section6.Node;
 
-public class Solution62 {
+public class Solution9 {
     // Tree 말단노드까지의 가장 짧은 경로 DFS
     Node root;
     public int DFS(int L, Node root){
@@ -14,7 +14,7 @@ public class Solution62 {
 
 
     public static void main ( String[] args ) {
-        Solution62 tree = new Solution62();
+        Solution9 tree = new Solution9();
         tree.root = new Node( 1 );
         tree.root.lt = new Node( 2 );
         tree.root.rt = new Node( 3 );
