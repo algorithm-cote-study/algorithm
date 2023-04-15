01aa6f41fa8fe676c0e857a2a62bbd35d9d19809
leeseunggu <leesg6107@gmail.com>
2023-04-15 14:24:41 +0900

    feat: 백준 최대힙 문제 풀이
    ```
    [최대 힙](https://www.acmicpc.net/problem/11279)
    ```

---
+++ src/main/java/sgyj/backjun/seunggu/Main11279.java
@@ -0,0 +1,35 @@
+package sgyj.backjun.seunggu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main11279 {
+
+    public static void main(String[] args) {
+        try( BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+            int count = Integer.parseInt(reader.readLine());
+            StringBuilder stringBuilder =new StringBuilder();
+            for(int i=0; i<count; i++) {
+                int num = Integer.parseInt(reader.readLine());
+                if(num == 0) {
+                    if(queue.isEmpty()) {
+                        stringBuilder.append(0);
+                    } else {
+                        stringBuilder.append(queue.poll());
+                    }
+                    stringBuilder.append("\n");
+                } else {
+                    queue.offer(num);
+                }
+            }
+            System.out.println(stringBuilder);
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
